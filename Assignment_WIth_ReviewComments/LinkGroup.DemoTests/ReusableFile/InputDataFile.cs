c310aad8068ed8e09c805e235fa7cf3c02608ffd
varshapujar <33929257+varshapujar@users.noreply.github.com>
2021-06-25 13:58:07 +0530

    Demo

---
+++ Assignment_WIth_ReviewComments/LinkGroup.DemoTests/ReusableFile/InputDataFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkGroup.DemoTests.ReusableFile
+{
+    public class InputDataFile
+    {
+        private static InputDataFile instance;
+
+        public static InputDataFile getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new InputDataFile();
+            }
+            return instance;
+        }
+
+        public void closeInstance()
+        {
+            instance = null;
+        }
+        public string linkGroupUrl = "https://www.linkgroup.eu/";
+        public string expectedSearchResults = "Leeds";
+        public string linkFundSolutionsUrl = "https://www.linkfundsolutions.co.uk/";
+        public string expectedFundForUK = "UK";
+        public string expectedFundForSwiss = "Swiss";
+    }
+}
